b3e9ff9c0c2d3cc3c8451a839f4f264f5045176e
Kent Tsou <tsoukent97@gmail.com>
2022-07-28 19:05:37 +1200

    main page changed

--- 1. Frontend/my-app/src/App.tsx
+++ 1. Frontend/my-app/src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [pokemonName, setPokemonName] = useState("");
+
+  function search() {
+    alert("Search button has been clicked!");
+}
+
   return (
     <div>
+        <h1>Pokemon Search</h1>
+        <div>
+          <label>Pokemon Name</label><br/>
+          <input type="text" id="pokemon-name" name="pokemon-name" onChange={e => setPokemonName(e.target.value)}/><br/>
+          <button onClick={search}>
+            Search
+          </button>
+        </div>
         <p>
-            Hello, world
+        You have entered {pokemonName}
         </p>
     </div>
   );
 }
 
 export default App;
+
