fbd2ed427bcea98227a35a680b7084425d33653a
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-08-07 16:00:28 +1200

    Displaying retrieved monster data
    - Displaying details from monster search e.g. description when there is data successfully retrieved

--- 1. Frontend/monster-hunter-app/src/App.tsx
+++ 1. Frontend/monster-hunter-app/src/App.tsx
@@ -5,12 +5,13 @@ import './App.css';
 function App() {
   // State variables
   const [monsterName, setMonsterName] = useState("");
+  const [monsterData, setMonsterData] = useState<undefined | any>(undefined);
 
   const MONSTER_HUNTER_BASE_URL = "https://mhw-db.com"
   return (
     <div>
       <h1>
-        Monster Hunter Database: Monster Search
+        Monster Hunter World: Monster Search
       </h1>
       
       <div>
@@ -29,14 +30,23 @@ function App() {
         You have entered {monsterName}
       </p>
 
-      <div id="pokemon-result">Monster data here!</div>
+      {/* Display Monster Data if it exists */}
+      {monsterData === undefined ? (
+        <p>Monster not found</p>
+      ) : (
+        <div id="pokemon-result">
+          <p>Description: {monsterData.description}</p>
+          <p>Species: {monsterData.species}</p>
+        </div>
+      )}
     </div>
   );
 
+  // Monster Hunter API calling function
   function search(){
       axios.get(MONSTER_HUNTER_BASE_URL + "/monsters?q={\"name\":\"" + monsterName + "\"}")
       .then((res) => {
-        console.log(res.data);
+        setMonsterData(res.data[0]);
       })
   }
 }
